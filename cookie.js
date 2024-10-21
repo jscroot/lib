73392e68606f5fb423d1d0aa5bdaba2fac03ce94
Rolly Maulana Awangga <awangga@gmail.com>
2024-10-21 11:51:56 +0700

    double declaration

--- cookie.js
+++ cookie.js
@@ -19,13 +19,6 @@ export function setCookieWithExpireSecond(cname, cvalue, exsecs) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-export function setCookieWithExpireDay(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
 export function deleteCookie(cname) {
   document.cookie = cname + "= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
