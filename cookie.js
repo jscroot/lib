2b978183c20acf4b4e24a03544de7a2d657643a8
Rolly Maulana Awangga <awangga@gmail.com>
2024-10-21 11:46:25 +0700

    update cookie dengan expire day

--- cookie.js
+++ cookie.js
@@ -19,21 +19,30 @@ export function setCookieWithExpireSecond(cname, cvalue, exsecs) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+export function setCookieWithExpireDay(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 export function deleteCookie(cname) {
   document.cookie = cname + "= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
+
 export function getCookie(cname) {
   let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
   }
   return "";
 }
