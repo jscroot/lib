6e0a7991edec6c73d5db6ef0fe6b658bd0f97869
Rolly Maulana Awangga <awangga@gmail.com>
2024-10-25 05:30:52 +0700

    cookie kompatibel dengan karakter khusus

--- cookie.js
+++ cookie.js
@@ -1,40 +1,44 @@
+// Fungsi untuk menyimpan cookie dengan hari kadaluwarsa
 export function setCookieWithExpireDay(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = encodeURIComponent(cname) + "=" + encodeURIComponent(cvalue) + ";" + expires + ";path=/";
+}
+
+// Fungsi untuk menyimpan cookie dengan jam kadaluwarsa
 export function setCookieWithExpireHour(cname, cvalue, exhour) {
   const d = new Date();
   d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = encodeURIComponent(cname) + "=" + encodeURIComponent(cvalue) + ";" + expires + ";path=/";
 }
 
+// Fungsi untuk menyimpan cookie dengan detik kadaluwarsa
 export function setCookieWithExpireSecond(cname, cvalue, exsecs) {
   const d = new Date();
   d.setTime(d.getTime() + (exsecs * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = encodeURIComponent(cname) + "=" + encodeURIComponent(cvalue) + ";" + expires + ";path=/";
 }
 
+// Fungsi untuk menghapus cookie dengan cara mengatur waktu kadaluwarsa ke masa lalu
 export function deleteCookie(cname) {
-  document.cookie = cname + "= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = encodeURIComponent(cname) + "= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
-
+// Fungsi untuk mendapatkan nilai cookie berdasarkan nama
 export function getCookie(cname) {
-  let name = cname + "=";
+  let name = encodeURIComponent(cname) + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  for(let i = 0; i < ca.length; i++) {
       let c = ca[i];
-      while (c.charAt(0) == ' ') {
+      while (c.charAt(0) === ' ') {
           c = c.substring(1);
       }
-      if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
+      if (c.indexOf(name) === 0) {
+          return decodeURIComponent(c.substring(name.length, c.length));
       }
   }
   return "";
